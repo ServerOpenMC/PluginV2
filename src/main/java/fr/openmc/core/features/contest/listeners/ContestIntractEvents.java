2f9042a914638a2a4bc56670404a441360a6aedd
iambibi <lethiebaut2@gmail.com>
2024-12-23 19:36:07 +0100

    Borne fix

--- src/main/java/fr/openmc/core/features/contest/listeners/ContestIntractEvents.java
+++ src/main/java/fr/openmc/core/features/contest/listeners/ContestIntractEvents.java
@@ -9,7 +9,7 @@ import org.bukkit.Bukkit;
 public class ContestIntractEvents implements Listener {
     @EventHandler
     private void onFurnitureInteractEvent(FurnitureInteractEvent furniture) {
-        if (furniture.getNamespacedID().equals("contest:borne")) {
+        if (furniture.getNamespacedID().equals("omc_contest:borne")) {
             furniture.getPlayer().playSound(furniture.getPlayer().getLocation(), Sound.BLOCK_BARREL_OPEN, 1.0F, 0.7F);
             Bukkit.dispatchCommand(furniture.getPlayer(), "openmc:contest");
         }
