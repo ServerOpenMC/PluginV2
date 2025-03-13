9c6cd11dbeb6e9fdf757ef7bc0c977c657ef2e6f
iambibi <lethiebaut2@gmail.com>
2025-03-13 19:37:22 +0100

    Add comptability with Free claim

--- src/main/java/fr/openmc/core/features/city/menu/CityChunkMenu.java
+++ src/main/java/fr/openmc/core/features/city/menu/CityChunkMenu.java
@@ -8,6 +8,7 @@ import fr.openmc.core.features.city.CPermission;
 import fr.openmc.core.features.city.City;
 import fr.openmc.core.features.city.CityManager;
 import fr.openmc.core.features.city.commands.CityCommands;
+import fr.openmc.core.features.city.mascots.MascotsManager;
 import fr.openmc.core.utils.cooldown.DynamicCooldownManager;
 import fr.openmc.core.utils.menu.ConfirmMenu;
 import fr.openmc.core.utils.messages.MessageType;
@@ -120,12 +121,12 @@ public class CityChunkMenu extends Menu {
                         ConfirmMenu menu = new ConfirmMenu(
                                 player,
                                 () -> {
-                                    player.closeInventory();
                                     Bukkit.getScheduler().runTask(OMCPlugin.getInstance(), () -> {
                                         CityCommands.claim(player, chunkX, chunkZ);
                                     });
+                                    new CityChunkMenu(player).open();
                                 },
-                                () -> player.closeInventory(),
+                                () -> new CityChunkMenu(player).open(),
                                 List.of(Component.text("§7Voulez vous vraiment claim ce chunk ?")),
                                 List.of(Component.text("§7Annuler la procédure de claim")));
                         menu.open();
@@ -144,6 +145,16 @@ public class CityChunkMenu extends Menu {
             CityMenu menu = new CityMenu(player);
             menu.open();
         }));
+
+        if (MascotsManager.freeClaim.containsKey(city2.getUUID()) && MascotsManager.freeClaim.get(city2.getUUID())> 0) {
+            inventory.put(49, new ItemBuilder(this, Material.GOLD_BLOCK, itemMeta -> {
+                itemMeta.displayName(Component.text("§6Claim Gratuit"));
+                itemMeta.lore(List.of(Component.text("§7Vous avez §6" + MascotsManager.freeClaim.get(city2.getUUID())+ " claim gratuit !")));
+            }).setOnClick(event -> {
+                CityMenu menu = new CityMenu(player);
+                menu.open();
+            }));
+        }
 
         inventory.put(53, new ItemBuilder(this, Material.MAP, itemMeta -> {
             itemMeta.displayName(Component.text("§6Rafraîchir la carte"));
