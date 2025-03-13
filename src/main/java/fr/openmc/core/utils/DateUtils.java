04ff0462eed8b586c21cb574375a57facb90c756
iambibi <lethiebaut2@gmail.com>
2025-03-13 16:28:24 +0100

    Add convertor second to Xj Xh Xm Xs

--- src/main/java/fr/openmc/core/utils/DateUtils.java
+++ src/main/java/fr/openmc/core/utils/DateUtils.java
@@ -27,6 +27,19 @@ public class DateUtils {
         return String.format("%dj %dh %dm %ds", days, hours, minutes, seconds);
     }
 
+    public static String convertSecondToTime(long seconds) {
+
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+        long days = hours / 24;
+
+        seconds = seconds % 60;
+        minutes = minutes % 60;
+        hours = hours % 24;
+
+        return String.format("%dj %dh %dm %ds", days, hours, minutes, seconds);
+    }
+
         public static String formatRelativeDate(LocalDateTime dateTime) {
             LocalDateTime now = LocalDateTime.now();
             Duration duration = Duration.between(dateTime, now);
