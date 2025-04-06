ded1309755bcbec5ae453baed8c0d6895637bdd2
Gabriel <81768313+gab4000@users.noreply.github.com>
2025-04-06 16:03:55 +0200

    Update src/main/java/fr/openmc/core/utils/freeze/FreezeManager.java
    Co-authored-by: ltuffery <123221865+ltuffery@users.noreply.github.com>

--- src/main/java/fr/openmc/core/utils/freeze/FreezeManager.java
+++ src/main/java/fr/openmc/core/utils/freeze/FreezeManager.java
@@ -23,16 +23,19 @@ public class FreezeManager {
 		if (target == null) {
 			MessagesManager.sendMessage(player, Component.text("§4Joueur introuvable"), Prefix.OPENMC, MessageType.ERROR, false);
 		} else {
+			String freezed = "";
+
 			if (FROZEN_PLAYERS.contains(target)) {
 				FROZEN_PLAYERS.remove(target);
-				MessagesManager.sendMessage(player, Component.text("§2Vous avez unfreeze §6" + target.getName()), Prefix.OPENMC, MessageType.SUCCESS, false);
-				MessagesManager.sendMessage(target, Component.text("§2Vous avez été unfreeze"), Prefix.OPENMC, MessageType.INFO, true);
+				freezed = "freeze";
 			} else {
 				FROZEN_PLAYERS.add(target);
 				target.sendTitle("§4Vous êtes freeze", "§5Si vous vous déconnectez, vous serez banni");
-				MessagesManager.sendMessage(player, Component.text("§2Vous avez freeze §6" + target.getName()), Prefix.OPENMC, MessageType.SUCCESS, false);
-				MessagesManager.sendMessage(target, Component.text("§4Vous avez été freeze"), Prefix.OPENMC, MessageType.WARNING, true);
+				freezed = "unfreeze";
 			}
+
+			MessagesManager.sendMessage(player, Component.text("§2Vous avez " + freezed + "§6" + target.getName()), Prefix.OPENMC, MessageType.SUCCESS, false);
+			MessagesManager.sendMessage(target, Component.text("§4Vous avez été " + freezed), Prefix.OPENMC, MessageType.WARNING, true);
 		}
 	}
 }
