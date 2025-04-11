b82a5afbaaaa0018af0b31f08ab4fbbc3118f6e1
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-04-11 18:24:12 +0200

    feat: add /bank deposit and /bank withdraw commands

--- src/main/java/fr/openmc/core/features/economy/commands/BankCommands.java
+++ src/main/java/fr/openmc/core/features/economy/commands/BankCommands.java
@@ -2,9 +2,11 @@ package fr.openmc.core.features.economy.commands;
 
 import org.bukkit.entity.Player;
 
+import fr.openmc.core.features.economy.BankManager;
 import fr.openmc.core.features.economy.menu.PersonalBankMenu;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.DefaultFor;
+import revxrsal.commands.annotation.Subcommand;
 
 @Command({"bank", "banque"})
 public class BankCommands {
@@ -12,5 +14,15 @@ public class BankCommands {
     @DefaultFor("~")
     void openBankMenu(Player player) {
         new PersonalBankMenu(player).open();
+    }
+
+    @Subcommand("deposit")
+    void deposit(Player player, String input) {
+        BankManager.getInstance().addBankBalance(player, input);
+    }
+
+    @Subcommand("withdraw")
+    void withdraw(Player player, String input) {
+        BankManager.getInstance().withdrawBankBalance(player, input);
     }
 }
