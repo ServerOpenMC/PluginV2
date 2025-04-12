66fbc253d26a434334cf9ae47093068092f159ea
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-04-12 09:29:49 +0200

    feat: /bank bal

--- src/main/java/fr/openmc/core/features/economy/commands/BankCommands.java
+++ src/main/java/fr/openmc/core/features/economy/commands/BankCommands.java
@@ -3,13 +3,18 @@ package fr.openmc.core.features.economy.commands;
 import org.bukkit.entity.Player;
 
 import fr.openmc.core.features.economy.BankManager;
+import fr.openmc.core.features.economy.EconomyManager;
 import fr.openmc.core.features.economy.menu.PersonalBankMenu;
+import fr.openmc.core.utils.messages.MessageType;
+import fr.openmc.core.utils.messages.MessagesManager;
+import fr.openmc.core.utils.messages.Prefix;
+import net.kyori.adventure.text.Component;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.DefaultFor;
 import revxrsal.commands.annotation.Description;
 import revxrsal.commands.annotation.Subcommand;
 
-@Command({"bank", "banque"})
+@Command({ "bank", "banque" })
 public class BankCommands {
 
     @DefaultFor("~")
@@ -28,5 +33,11 @@ public class BankCommands {
     @Description("Retire de l'argent de votre banque personelle")
     void withdraw(Player player, String input) {
         BankManager.getInstance().withdrawBankBalance(player, input);
+    }
+
+    @Subcommand({ "balance", "bal" })
+    void withdraw(Player player) {
+        double balance = BankManager.getInstance().getBankBalance(player.getUniqueId());
+        MessagesManager.sendMessage(player, Component.text("Il y a §d" + EconomyManager.getInstance().getFormattedSimplifiedNumber(balance) + "§r" + EconomyManager.getEconomyIcon() + " dans ta banque"), Prefix.CITY, MessageType.INFO, false);
     }
 }
