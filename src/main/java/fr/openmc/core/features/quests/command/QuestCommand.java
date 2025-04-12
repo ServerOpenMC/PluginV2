07966dde7986c857dabd5b82e78bd014819d099e
axenodev <tbavart@gmail.com>
2025-04-12 16:37:39 +0200

    Adding target in menu

--- src/main/java/fr/openmc/core/features/quests/command/QuestCommand.java
+++ src/main/java/fr/openmc/core/features/quests/command/QuestCommand.java
@@ -1,10 +1,11 @@
 package fr.openmc.core.features.quests.command;
 
 import fr.openmc.core.features.quests.menus.QuestsMenu;
+import fr.openmc.core.utils.messages.MessageType;
+import fr.openmc.core.utils.messages.MessagesManager;
+import fr.openmc.core.utils.messages.Prefix;
 import org.bukkit.entity.Player;
-import revxrsal.commands.annotation.Command;
-import revxrsal.commands.annotation.DefaultFor;
-import revxrsal.commands.annotation.Description;
+import revxrsal.commands.annotation.*;
 
 @Command({"quest"})
 @Description("Commande pour les quêtes")
@@ -13,6 +14,18 @@ public class QuestCommand {
     @DefaultFor({"~"})
     @Description("Ouvre le menu des quêtes")
     public void onQuest(Player player) {
-        (new QuestsMenu(player)).open();
+        new QuestsMenu(player).open();
+    }
+
+    @Subcommand("open")
+    @Description("Ouvre le menu des quêtes")
+    public void resetProgress(Player sender, @Optional Player target) {
+        if (target == null || target == sender) {
+            new QuestsMenu(sender).open();
+        } else {
+            if (sender.hasPermission("omc.quests.admin")) new QuestsMenu(target).open();
+            else MessagesManager.sendMessage(sender, MessagesManager.Message.NOPERMISSION.getMessage(), Prefix.OPENMC, MessageType.ERROR, true);
+
+        }
     }
 }
