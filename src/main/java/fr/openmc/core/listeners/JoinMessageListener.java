167f7c30a4c574ef40412c6137ab2947693ba214
Xernas78 <71571183+Xernas78@users.noreply.github.com>
2025-04-18 20:19:23 +0200

    Removed unused import

--- src/main/java/fr/openmc/core/listeners/JoinMessageListener.java
+++ src/main/java/fr/openmc/core/listeners/JoinMessageListener.java
@@ -15,7 +15,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.UUID;
