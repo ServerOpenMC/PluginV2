0e7e0422470998b74d2ecca5fc05aef2a67907d6
gab4000 <gabriel.cosme@orange.fr>
2025-04-19 15:43:27 +0200

    Retrait d'une ligne dans le scoreboard

--- src/main/java/fr/openmc/core/features/scoreboards/ScoreboardManager.java
+++ src/main/java/fr/openmc/core/features/scoreboards/ScoreboardManager.java
@@ -139,8 +139,7 @@ public class ScoreboardManager implements Listener {
         for (String entry : scoreboard.getEntries()) {
             scoreboard.resetScores(entry);
         }
-
-        objective.getScore(" §8------------- ").setScore(11);
+        
         objective.getScore("§8• §fNom: §7"+player.getName()).setScore(10);
 
         City city = CityManager.getPlayerCity(player.getUniqueId());
