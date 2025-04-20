db2a2bb80acebe7f4c20775a883a92997d8527cf
misieur <misieur.off@gmail.com>
2025-04-20 11:12:38 +0200

    Amélioration de RTP

--- src/main/java/fr/openmc/core/OMCPlugin.java
+++ src/main/java/fr/openmc/core/OMCPlugin.java
@@ -2,7 +2,6 @@ package fr.openmc.core;
 
 import dev.xernas.menulib.MenuLib;
 import fr.openmc.core.commands.CommandsManager;
-import fr.openmc.core.commands.utils.rtpManager;
 import fr.openmc.core.features.scoreboards.ScoreboardManager;
 import fr.openmc.core.features.city.CityManager;
 import fr.openmc.core.features.city.mascots.MascotsManager;
@@ -62,7 +61,6 @@ public class OMCPlugin extends JavaPlugin {
         ContestManager contestManager = new ContestManager(this);
         ContestPlayerManager contestPlayerManager = new ContestPlayerManager();
         new SpawnManager(this);
-        new rtpManager(this);
         new MascotsManager(this); // laisser avant CityManager
         new CityManager();
         new ListenersManager();
--- src/main/java/fr/openmc/core/commands/utils/Rtp.java
+++ src/main/java/fr/openmc/core/commands/utils/Rtp.java
@@ -4,83 +4,113 @@ import fr.openmc.core.OMCPlugin;
 import fr.openmc.core.utils.cooldown.DynamicCooldown;
 import fr.openmc.core.utils.cooldown.DynamicCooldownManager;
 import me.clip.placeholderapi.PlaceholderAPI;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.Description;
 import revxrsal.commands.bukkit.annotation.CommandPermission;
 
+import java.io.File;
+
 public class Rtp {
+
+    private final File rtpFile;
+    private int minRadius;
+    private int maxRadius;
+    private int maxTries;
+    private int rtpCooldown;
+
+    public Rtp() {
+        this.rtpFile = new File(OMCPlugin.getInstance().getDataFolder() + "/data", "rtp.yml");
+        loadRTPConfig();
+    }
+
+    private void loadRTPConfig() {
+        if (!rtpFile.exists()) {
+            rtpFile.getParentFile().mkdirs();
+            OMCPlugin.getInstance().saveResource("data/rtp.yml", false);
+        }
+
+        FileConfiguration rtpConfig = YamlConfiguration.loadConfiguration(rtpFile);
+        this.maxRadius = rtpConfig.getInt("max-radius");
+        this.minRadius = rtpConfig.getInt("min-radius");
+        this.maxTries = rtpConfig.getInt("max-tries");
+        this.rtpCooldown = rtpConfig.getInt("rtp-cooldown");
+    }
+
     @Command("rtp")
     @Description("Permet de se téléporter à un endroit aléatoire")
     @CommandPermission("omc.commands.rtp")
     @DynamicCooldown(group = "rpt")
     public void rtp(CommandSender sender) {
-
         if (sender instanceof Player player) {
-            World world = player.getWorld();
-            int minRadius = rtpManager.getInstance().getMinRadius();
-            int maxRadius = rtpManager.getInstance().getMaxRadius();
-            int maxTries = rtpManager.getInstance().getMaxTries();
-            int rtpCooldown = rtpManager.getInstance().getRtpCooldown();
-
-            boolean result = rtpPlayer(player);
-            if (!result) {
-                player.sendActionBar("RTP: Tentative 1/" + maxTries + " §cÉchec§r...");
-                for (int i = 1; i < maxTries; i++) {
-                    // Attendre une seconde avant de réessayer
-                    try {
-                        Thread.sleep(1000); // C'est un tread asynchrone, donc pas de problème (normalement)
-                    } catch (InterruptedException e) {
-                        Bukkit.getLogger().severe(e.getMessage());
-                    }
-                    result = rtpPlayer(player);
-                    if (!result) {
-                        player.sendActionBar("RTP: Tentative " + i + "/" + maxTries + " §cÉchec§r...");
-                    } else {
-                        DynamicCooldownManager.use(player.getUniqueId(), "rpt", rtpCooldown);
-                        return;
-                    }
-
-                }
-            } else {
-                DynamicCooldownManager.use(player.getUniqueId(), "rpt", rtpCooldown);
-                return;
-            }
-            player.sendActionBar("Échec du RTP réessayez plus tard...");
-            DynamicCooldownManager.use(player.getUniqueId(), "rpt", 15);
+            DynamicCooldownManager.use(player.getUniqueId(), "rpt", 1000 * 15); // Pour être sûr que le jouer ne réexécute pas la commande avant qu'elle soit finie
+            rtpPlayer(player, 0);
         }
     }
-    public boolean rtpPlayer(Player player) {
-        World world = player.getWorld();
-        int minRadius = rtpManager.getInstance().getMinRadius();
-        int maxRadius = rtpManager.getInstance().getMaxRadius();
-        int maxTries = rtpManager.getInstance().getMaxTries();
-        int rtpCooldown = rtpManager.getInstance().getRtpCooldown();
-        int radius = (int) (Math.random() * (maxRadius - minRadius + 1)) + minRadius;
-        float angle = (float) (Math.random() * 2 * Math.PI);
-        int x = (int) (Math.cos(angle) * radius);
-        int z = (int) (Math.sin(angle) * radius);
-        Location loc = world.getHighestBlockAt(x, z).getLocation();
 
-        if (loc.getBlock().isSolid() && loc.getBlockY() > 0) {
-            loc.setY(loc.getY() + 1);
-            player.sendTitle(PlaceholderAPI.setPlaceholders(player, "§0%img_tp_effect%"), "§a§lTéléportation...", 20, 10, 10);
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    player.teleport(loc);
-                    player.sendMessage(PlaceholderAPI.setPlaceholders(player, "§aVous avez été téléporté à §6X: §e" + loc.getBlockX() + "§6, Y:§e" + loc.getBlockY() + "§6, Z: §e" + loc.getBlockZ()));
+    private void rtpPlayer(Player player, int tries) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (tryRtp(player)) {
+                    DynamicCooldownManager.use(player.getUniqueId(), "rpt", 1000L * rtpCooldown);
+                } else {
+                    if (tries < maxTries) {
+                        player.sendActionBar("RTP: Tentative " + (tries + 1) + "/" + maxTries + " §cÉchec§r...");
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                rtpPlayer(player, tries + 1);
+                            }
+                        }.runTaskLaterAsynchronously(OMCPlugin.getInstance(), 20);
+                    } else {
+                        player.sendActionBar("Échec du RTP réessayez plus tard...");
+                        // On a déjà mis le cooldown au début
+                    }
                 }
-            }.runTaskLater(OMCPlugin.getInstance(), 10);
+            }
+        }.runTaskAsynchronously(OMCPlugin.getInstance());
+
+    }
+
+    public boolean tryRtp(Player player) {
+        Location loc = generateRandomLocation(player.getWorld());
+        if (isSafe(loc)) {
+            loc.add(0.5, 1, 0.5);
+            tpPlayer(player, loc);
             return true;
         } else {
             return false;
         }
+    }
+
+    public boolean isSafe(Location loc) {
+        return loc.getBlock().isSolid() && loc.getBlockY() > 50;
+    }
+
+    public Location generateRandomLocation(World world) {
+        int radius = (int) (Math.random() * (maxRadius - minRadius + 1)) + minRadius;
+        float angle = (float) (Math.random() * 2 * Math.PI);
+        int x = (int) (Math.cos(angle) * radius);
+        int z = (int) (Math.sin(angle) * radius);
+        return world.getHighestBlockAt(x, z).getLocation();
+    }
+
+    public void tpPlayer(Player player, Location loc) {
+        player.sendTitle(PlaceholderAPI.setPlaceholders(player, "§0%img_tp_effect%"), "§a§lTéléportation...", 20, 10, 20);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                player.teleport(loc);
+                player.sendMessage(PlaceholderAPI.setPlaceholders(player, "§aVous avez été téléporté à §6X: §e" + loc.getBlockX() + "§6, Y:§e" + loc.getBlockY() + "§6, Z: §e" + loc.getBlockZ()));
+            }
+        }.runTaskLater(OMCPlugin.getInstance(), 10);
     }
 
 }
