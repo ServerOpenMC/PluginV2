225d6aa683a32ebadddd27f8d3a13ef31b5e76a7
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-05-01 15:02:05 +0200

    fix /omc update description

--- src/main/java/fr/openmc/core/features/updates/UpdateCommand.java
+++ src/main/java/fr/openmc/core/features/updates/UpdateCommand.java
@@ -9,7 +9,7 @@ import revxrsal.commands.bukkit.annotation.CommandPermission;
 public class UpdateCommand {
     @Command("omc version")
     @CommandPermission("omc.commands.version")
-    @Description("Faire un lancé de dés (Donne un nombre aléatoire entre 1 et 10)")
+    @Description("Vous donne de l'information sur le version du plugin")
     private void version(Player player) {
         UpdateManager.getInstance().sendUpdateMessage(player);
     }
