9004bfff1271173b86ea2079927489d7ed8b625a
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-05-01 15:01:32 +0200

    fix: permission for omc update command

--- src/main/java/fr/openmc/core/features/updates/UpdateCommand.java
+++ src/main/java/fr/openmc/core/features/updates/UpdateCommand.java
@@ -8,7 +8,7 @@ import revxrsal.commands.bukkit.annotation.CommandPermission;
 
 public class UpdateCommand {
     @Command("omc version")
-    @CommandPermission("omc.commands.diceroll")
+    @CommandPermission("omc.commands.version")
     @Description("Faire un lancé de dés (Donne un nombre aléatoire entre 1 et 10)")
     private void version(Player player) {
         UpdateManager.getInstance().sendUpdateMessage(player);
