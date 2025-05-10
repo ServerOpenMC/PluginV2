a08a8eb5d54bbb38eff41319dc40773e6bb1c292
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-05-10 10:28:47 +0200

    add: try CitizensAPI (4)

--- src/main/java/fr/openmc/core/listeners/CitizensHookListener.java
+++ src/main/java/fr/openmc/core/listeners/CitizensHookListener.java
@@ -4,14 +4,15 @@ import fr.openmc.core.OMCPlugin;
 import fr.openmc.core.utils.api.CitizensApi;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.event.CitizensEnableEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class CitizensHookListener implements Listener {
     @EventHandler
     public void onCitizensEnable(CitizensEnableEvent event) {
-        Bukkit.getLogger().info("[OpenMC] Citizens est chargé, l’API est prête.");
+        System.out.println(event.getHandlers());
+        System.out.println("CitizensEnableEvent");
+        System.out.println("[OpenMC] Citizens est chargé, l’API est prête.");
         System.out.println(OMCPlugin.getInstance().getServer().getServicesManager().load(CitizensAPI.class));
         CitizensApi.setHasCitizens(true);
     }
