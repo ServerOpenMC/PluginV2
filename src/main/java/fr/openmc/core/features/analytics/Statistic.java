0c8407566d2c51aed1c73fc200ea98691ccf92a4
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-05-10 19:34:56 +0200

    create stats table in DB

---
+++ src/main/java/fr/openmc/core/features/analytics/Statistic.java
@@ -0,0 +1,27 @@
+package fr.openmc.core.features.analytics;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import lombok.Getter;
+
+@Getter
+@DatabaseTable(tableName = "stats")
+class Statistic {
+    Statistic() {
+        // required for ORMLite
+    }
+
+    Statistic(String player, String scope, int value) {
+        this.player = player;
+        this.scope = scope;
+        this.value = value;
+    }
+
+    @DatabaseField(canBeNull = false)
+    private String player;
+    @DatabaseField(canBeNull = false)
+    public String scope;
+    @DatabaseField(canBeNull = false, defaultValue = "0")
+    private int value;
+}
