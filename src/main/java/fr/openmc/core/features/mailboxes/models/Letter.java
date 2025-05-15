3add8ce2e25c31f9745d6c648daffe877dc3b2ff
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-05-15 19:15:30 +0200

    finish Letter class

--- src/main/java/fr/openmc/core/features/mailboxes/models/Letter.java
+++ src/main/java/fr/openmc/core/features/mailboxes/models/Letter.java
@@ -1,21 +1,47 @@
 package fr.openmc.core.features.mailboxes.models;
 
+import java.sql.Date;
+
+import com.j256.ormlite.field.DataType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = "mail")
-class Mail {
+public class Letter {
     @DatabaseField(id = true, generatedId = true)
     private int id;
     @DatabaseField(canBeNull = false)
     private String sender;
     @DatabaseField(canBeNull = false)
     private String receiver;
-    // "items" BLOB NOT NULL
-    // @DatabaseField(canBeNull = false)
-    // pivate Blob items;
+    @DatabaseField(canBeNull = false)
+    private byte[] items;
+    @DatabaseField(columnName = "num_items", canBeNull = false)
+    private int numItems;
+    @DatabaseField(dataType = DataType.DATE_STRING, format = "yyyy-MM-dd HH:mm:ss", columnDefinition = "TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP")
+    private Date sent;
+    @DatabaseField
+    private boolean refused;
 
-    Mail() {
+    Letter() {
         // required by ORMLite
+    }
+
+    Letter(String sender, String receiver, byte[] items, int numItems, boolean refused) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.items = items;
+        this.numItems = numItems;
+        this.refused = refused;
+    }
+
+    Letter(int id, String sender, String receiver, byte[] items, int numItems, Date sent, boolean refused) {
+        this.id = id;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.items = items;
+        this.numItems = numItems;
+        this.sent = sent;
+        this.refused = refused;
     }
 }
