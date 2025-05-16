a3d5833e252c539d0e07107041f5e6995e0d1c12
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-05-16 20:07:03 +0200

    add Player model for economy ORM integration

---
+++ src/main/java/fr/openmc/core/features/economy/models/Player.java
@@ -0,0 +1,35 @@
+package fr.openmc.core.features.economy.models;
+
+import java.util.UUID;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import lombok.Getter;
+
+@Getter
+@DatabaseTable(tableName = "banks")
+public class Player {
+    @DatabaseField(id = true)
+    private UUID player;
+    @DatabaseField(canBeNull = false, defaultValue = "0")
+    private double balance;
+
+    Player() {
+        // necessary for OrmLite
+    }
+
+    public Player(UUID player) {
+        this.player = player;
+        this.balance = 0;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+        assert balance >= 0;
+    }
+}
