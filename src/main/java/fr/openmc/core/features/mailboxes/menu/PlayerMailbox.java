b1d037b3d4049ac60492c5d67fb22ad5642cc9a3
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-05-16 19:54:45 +0200

    convert PlayerMailbox

--- src/main/java/fr/openmc/core/features/mailboxes/menu/PlayerMailbox.java
+++ src/main/java/fr/openmc/core/features/mailboxes/menu/PlayerMailbox.java
@@ -1,6 +1,7 @@
 package fr.openmc.core.features.mailboxes.menu;
 
-
+import fr.openmc.core.features.mailboxes.Letter;
+import fr.openmc.core.features.mailboxes.MailboxManager;
 import fr.openmc.core.features.mailboxes.letter.LetterHead;
 import fr.openmc.core.features.mailboxes.utils.PaginatedMailbox;
 import fr.openmc.core.utils.database.DatabaseManager;
@@ -11,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public class PlayerMailbox extends PaginatedMailbox<LetterHead> {
@@ -21,13 +23,15 @@ public class PlayerMailbox extends PaginatedMailbox<LetterHead> {
 
     public PlayerMailbox(Player player) {
         super(player);
-        if (fetchMailbox()) initInventory();
+        if (fetchMailbox())
+            initInventory();
     }
 
     public void addLetter(LetterHead letterHead) {
         pageItems.add(letterHead);
         int size = pageItems.size();
-        if (size - 1 / maxIndex == page) updateInventory(false, size - 1 % maxIndex);
+        if (size - 1 / maxIndex == page)
+            updateInventory(false, size - 1 % maxIndex);
     }
 
     public void removeLetter(int id) {
@@ -47,21 +51,11 @@ public class PlayerMailbox extends PaginatedMailbox<LetterHead> {
     }
 
     public boolean fetchMailbox() {
-        try (PreparedStatement statement = DatabaseManager.getConnection().prepareStatement("SELECT id, sender_id, sent_at, items_count FROM mailbox_items WHERE receiver_id = ? AND refused = false ORDER BY sent_at DESC;")) {
-            statement.setString(1, player.getUniqueId().toString());
-            try (ResultSet result = statement.executeQuery()) {
-                while (result.next()) {
-                    int id = result.getInt("id");
-                    UUID senderUUID = UUID.fromString(result.getString("sender_id"));
-                    int itemsCount = result.getInt("items_count");
-                    LocalDateTime sentAt = result.getTimestamp("sent_at").toLocalDateTime();
-                    pageItems.add(new LetterHead(Bukkit.getOfflinePlayer(senderUUID), id, itemsCount, sentAt));
-                }
-                return true;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        List<Letter> letters = MailboxManager.getLetters(player);
+        if (letters.size() < 1)
             return false;
-        }
+
+        letters.forEach((letter) -> pageItems.add(letter.toLetterHead()));
+        return true;
     }
 }
