30a091b902f3c5de2579400d4ac67872a89b5b23
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-05-17 16:15:47 +0200

    create friend model

---
+++ src/main/java/fr/openmc/core/features/friend/Friend.java
@@ -0,0 +1,29 @@
+package fr.openmc.core.features.friend;
+
+import java.sql.Date;
+import java.util.UUID;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@DatabaseTable(tableName = "friends")
+public class Friend {
+    @DatabaseField(canBeNull = false)
+    private UUID first;
+    @DatabaseField(canBeNull = false)
+    private UUID second;
+    @DatabaseField(dataType = DataType.DATE_STRING, format = "yyyy-MM-dd HH:mm:ss", columnDefinition = "TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP")
+    private Date date;
+    @Setter
+    @DatabaseField(columnName = "best_friend")
+    private boolean bestFriend;
+
+    Friend() {
+        // required for ORMLite
+    }
+}
