ae477f4ba06b72794ecb7dd089f692068879927a
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-05-17 16:05:20 +0200

    convert Homemanager to orm

--- src/main/java/fr/openmc/core/features/homes/models/HomeLimit.java
+++ src/main/java/fr/openmc/core/features/homes/models/HomeLimit.java
@@ -5,23 +5,41 @@ import java.util.UUID;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+import fr.openmc.core.features.homes.HomeLimits;
 import lombok.Getter;
+import lombok.Setter;
 
 @DatabaseTable(tableName = "homes")
-public class Limit {
+public class HomeLimit {
 
+    @Getter
     @DatabaseField(id = true)
     private UUID player;
     @Getter
+    @Setter
     @DatabaseField(canBeNull = false)
     private int limit;
 
-    Limit() {
+    HomeLimit() {
         // required for ORMLite
     }
 
-    public Limit(UUID player, int limit) {
+    public HomeLimit(UUID player, int limit) {
         this.player = player;
         this.limit = limit;
+    }
+
+    public HomeLimit(UUID player, HomeLimits limit) {
+        this.player = player;
+        this.limit = limit.getLimit();
+    }
+
+    public HomeLimits getHomeLimit() {
+        for (HomeLimits value : HomeLimits.values()) {
+            if (value.getLimit() == this.limit) {
+                return value;
+            }
+        }
+        return null;
     }
 }
