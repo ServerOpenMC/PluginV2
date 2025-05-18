ad05824c98cccb65a23f1e6d78ffa2f84c18cbe8
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-05-18 11:41:52 +0200

    rename merchant data

--- src/main/java/fr/openmc/core/features/corporation/models/MerchantDBData.java
+++ src/main/java/fr/openmc/core/features/corporation/models/MerchantDBData.java
@@ -6,13 +6,13 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = "merchant_data")
-public class MerchantData {
+public class MerchantDBData {
     @DatabaseField(id = true)
     private UUID id;
     @DatabaseField
     private byte[] content;
 
-    MerchantData() {
+    MerchantDBData() {
         // required for ORMLite
     }
 }
