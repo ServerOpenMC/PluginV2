d1003e121aa62bd1968be70ff4eb45ec01e76432
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-05-30 14:34:05 +0200

    fix: MascotMenu

--- src/main/java/fr/openmc/core/features/city/menu/mascots/MascotMenu.java
+++ src/main/java/fr/openmc/core/features/city/menu/mascots/MascotMenu.java
@@ -194,9 +194,9 @@ public class MascotMenu extends Menu {
 
                                 DynamicCooldownManager.use(mascot.getMascotUUID().toString(), "mascots:move", 5 * 3600 * 1000L);
                                 return true;
-                            }
-                            );
-                    ;
+                            },
+                            null
+                    );
                     player.closeInventory();
                 });
             };
@@ -208,7 +208,7 @@ public class MascotMenu extends Menu {
             }
 
             List<Component> requiredAmount = new ArrayList<>();
-            MascotsLevels mascotsLevels = MascotsLevels.valueOf("level" + mascot);
+            MascotsLevels mascotsLevels = MascotsLevels.valueOf("level" + mascot.getLevel());
 
             if (mascotsLevels.equals(MascotsLevels.level10)){
                 requiredAmount.add(Component.text("§7Niveau max atteint"));
