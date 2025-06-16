ca68ba939bfe409bf3b2cf413dbdc2bde5a12c61
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-06-16 11:31:47 +0200

    fix city chest

--- src/main/java/fr/openmc/core/features/city/commands/CityChestCommand.java
+++ src/main/java/fr/openmc/core/features/city/commands/CityChestCommand.java
@@ -68,7 +68,7 @@ public class CityChestCommand {
 
         city.updateBalance((double) -price);
 
-        city.saveChestContent(city.getChestPages(), null);
+        city.saveChestContent(city.getChestPages() + 1, null);
         MessagesManager.sendMessage(player, Component.text("Le coffre a été amélioré"), Prefix.CITY, MessageType.SUCCESS, false);
     }
 }
