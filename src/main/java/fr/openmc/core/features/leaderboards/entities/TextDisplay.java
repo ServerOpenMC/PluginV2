12bcb7d96f629f94bc48fb64ff8f8284558d0be5
misieur <misieur.off@mail.com>
2025-06-24 17:26:02 +0200

    Suppression d'imports

--- src/main/java/fr/openmc/core/features/leaderboards/entities/TextDisplay.java
+++ src/main/java/fr/openmc/core/features/leaderboards/entities/TextDisplay.java
@@ -10,7 +10,6 @@ import net.minecraft.network.protocol.game.ClientboundTeleportEntityPacket;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Brightness;
 import net.minecraft.world.entity.Display;
