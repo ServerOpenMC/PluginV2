74c84f187e080933e7165e92031baebbf343c5f4
Toutouchien <toutouchienshiba@gmail.com>
2025-07-03 18:29:18 +0200

    Make older items in DB compatible

--- src/main/java/fr/openmc/core/utils/serializer/BukkitSerializer.java
+++ src/main/java/fr/openmc/core/utils/serializer/BukkitSerializer.java
@@ -1,13 +1,27 @@
 package fr.openmc.core.utils.serializer;
 
+import lombok.Cleanup;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class BukkitSerializer {
-    public static byte[] serializeItemStacks(ItemStack[] inv) {
-        return ItemStack.serializeItemsAsBytes(inv);
+    public static byte[] serializeItemStacks(ItemStack[] inv) throws IOException {
+        // TODO: Replace by ItemStack.serializeItemsAsBytes(inv)
+        @Cleanup ByteArrayOutputStream os = new ByteArrayOutputStream();
+        @Cleanup BukkitObjectOutputStream bos = new BukkitObjectOutputStream(os);
+        bos.writeObject(inv);
+        return os.toByteArray();
     }
 
-    public static ItemStack[] deserializeItemStacks(byte[] b) {
-        return ItemStack.deserializeItemsFromBytes(b);
+    public static ItemStack[] deserializeItemStacks(byte[] b) throws Exception {
+        // TODO: Replace by ItemStack.deserializeItemsFromBytes(b)
+        @Cleanup ByteArrayInputStream bais = new ByteArrayInputStream(b);
+        @Cleanup BukkitObjectInputStream bois = new BukkitObjectInputStream(bais);
+        return (ItemStack[]) bois.readObject();
     }
 }
