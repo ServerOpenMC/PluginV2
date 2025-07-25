3789d502d229e77d94f40a2ffddc5d0ffc267cfd
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-07-25 17:17:56 +0200

    rewrite: speed modifier for happy ghast

--- src/main/java/fr/openmc/core/listeners/HappyGhastListener.java
+++ src/main/java/fr/openmc/core/listeners/HappyGhastListener.java
@@ -1,23 +1,36 @@
 package fr.openmc.core.listeners;
 
 import org.bukkit.attribute.Attribute;
-import org.bukkit.entity.HappyGhast;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.vehicle.VehicleEnterEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
 
 public class HappyGhastListener implements Listener {
     private final double DEFAULT_FLYING_SPEED = 0.05;
 
-    @EventHandler
-    public void onHappyGhastSpawn(VehicleEnterEvent event) {
-        System.out.println("deed");
-        if (event.getVehicle() instanceof HappyGhast mob) {
-            if (mob.getAttribute(Attribute.FLYING_SPEED) == null) {
-                return;
-            }
-            mob.getAttribute(Attribute.FLYING_SPEED).setBaseValue(DEFAULT_FLYING_SPEED * 1.5);
-        }
+    @EventHandler(ignoreCancelled = true)
+    public void onHappyGhastSpawn(EntitySpawnEvent event) {
+        if (event.getEntityType() != EntityType.HAPPY_GHAST) return;
+
+        LivingEntity entity = (LivingEntity) event.getEntity();
+
+        AttributeInstance attr = entity.getAttribute(Attribute.FLYING_SPEED);
+        if (attr == null) return;
+
+        double multiplier = getMultiplier();
+        attr.setBaseValue(DEFAULT_FLYING_SPEED * multiplier);
+    }
+
+    // Effet de rareté, plus c'est proche de 1.6, plus c'est rare
+    private double getMultiplier() {
+        double min = 1.0;
+        double max = 1.6;
+        double random = Math.random();
+        double biased = 1 - Math.pow(random, 3);
+        return min + (max - min) * biased;
     }
 
 }
