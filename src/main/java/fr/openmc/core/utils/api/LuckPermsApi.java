4990fc121bebd6035b525fb13ff94530b76b16bd
Toutouchien <toutouchienshiba@gmail.com>
2025-08-02 13:46:38 +0200

    Fix - iambibi review

--- src/main/java/fr/openmc/core/utils/api/LuckPermsApi.java
+++ src/main/java/fr/openmc/core/utils/api/LuckPermsApi.java
@@ -54,7 +54,7 @@ public class LuckPermsApi {
         String prefix = getPrefix(player);
         if (prefix == null || prefix.isEmpty()) return "";
         String formattedPrefix = prefix.replace("&", "§");
-        formattedPrefix = formattedPrefix.replaceAll(":([a-zA-Z0-9_]+):", ":$1%");
+        formattedPrefix = formattedPrefix.replaceAll(":([a-zA-Z0-9_]+):", ":$1:");
 
         return PlaceholderAPI.setPlaceholders(player, formattedPrefix) + " ";
     }
@@ -66,7 +66,7 @@ public class LuckPermsApi {
         if (prefix == null || prefix.isEmpty()) return Component.empty();
 
         String formattedPrefix = prefix.replace("&", "§");
-        formattedPrefix = formattedPrefix.replaceAll(":([a-zA-Z0-9_]+):", ":$1%");
+        formattedPrefix = formattedPrefix.replaceAll(":([a-zA-Z0-9_]+):", ":$1:");
 
         return Component.text(PlaceholderAPI.setPlaceholders(null, formattedPrefix) + " ");
     }
