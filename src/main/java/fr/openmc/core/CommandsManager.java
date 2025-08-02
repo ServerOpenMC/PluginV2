a91b21b8dbd51ead317de429cb65b1f7927d48f1
Toutouchien <toutouchienshiba@gmail.com>
2025-08-02 14:51:32 +0200

    FUKOUNA GIRL - STOMACH BOOK

--- src/main/java/fr/openmc/core/CommandsManager.java
+++ src/main/java/fr/openmc/core/CommandsManager.java
@@ -53,7 +53,8 @@ public class CommandsManager {
                 new MainMenuCommand(),
                 new PrivateMessageCommand(),
                 new SocialSpyCommand(),
-                new SettingsCommand());
+                new SettingsCommand()
+        );
     }
 
     private static void registerSuggestions() {
