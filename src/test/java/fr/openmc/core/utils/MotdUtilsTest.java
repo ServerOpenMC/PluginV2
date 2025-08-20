6eb9fe7058eb6e2ddcb97227144e3eb5ba15a0e9
Toutouchien <puppytransgirl+github@proton.me>
2025-08-20 21:08:55 +0200

    Fix tests ?

--- src/test/java/fr/openmc/core/utils/MotdUtilsTest.java
+++ src/test/java/fr/openmc/core/utils/MotdUtilsTest.java
@@ -1,43 +1,25 @@
 package fr.openmc.core.utils;
 
-import fr.openmc.core.OMCPlugin;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockbukkit.mockbukkit.MockBukkit;
-import org.mockbukkit.mockbukkit.ServerMock;
 
 class MotdUtilsTest {
+    @Test
+    @DisplayName("MOTD switch")
+    void testMOTD() {
+        String motd = getComponentContent(MockBukkit.getMock().motd());
 
-    private ServerMock server;
+        new MotdUtils();
+        MockBukkit.getMock().getScheduler().performTicks(12001L);
 
-    @BeforeEach
-    void setUp() {
-        server = MockBukkit.mock();
-
-        server.addSimpleWorld("world");
-
-        MockBukkit.load(OMCPlugin.class);
-    }
-
-    @AfterEach
-    void tearDown() {
-        MockBukkit.unmock();
+        Assertions.assertNotEquals(getComponentContent(MockBukkit.getMock().motd()), motd);
     }
 
     private String getComponentContent(Component component) {
         return ((TextComponent) component).content();
     }
-
-    @Test
-    @DisplayName("MOTD switch")
-    void testMOTD() {
-        String motd = getComponentContent(server.motd());
-
-        new MotdUtils();
-        server.getScheduler().performTicks(12001L);
-
-        Assertions.assertNotEquals(getComponentContent(server.motd()), motd);
-    }
-
 }
