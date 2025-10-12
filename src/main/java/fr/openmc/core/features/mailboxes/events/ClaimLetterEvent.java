e7de0c0a0a274142dcfe794858713eba3b1b844d
axeno <tbavart@gmail.com>
2025-10-12 12:34:07 +0200

    refactor: simplify ClaimLetterEvent by extending PlayerEvent

--- src/main/java/fr/openmc/core/features/mailboxes/events/ClaimLetterEvent.java
+++ src/main/java/fr/openmc/core/features/mailboxes/events/ClaimLetterEvent.java
@@ -1,19 +1,16 @@
 package fr.openmc.core.features.mailboxes.events;
 
-import lombok.Getter;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
 import org.jetbrains.annotations.NotNull;
 
-@Getter
-public class ClaimLetterEvent extends Event {
+public class ClaimLetterEvent extends PlayerEvent {
 
-    private final Player player;
     private static final HandlerList HANDLERS = new HandlerList();
 
     public ClaimLetterEvent(Player player) {
-        this.player = player;
+        super(player);
     }
 
     public static HandlerList getHandlerList() {
