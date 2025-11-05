e6da5e645191a4eeb473f5d9372f9b147e8cd72a
Alexis LEBRUN <alexis@lebrun.ovh>
2025-11-05 21:07:29 +0100

    fix import

--- src/main/java/fr/openmc/core/features/trashbin/Trash.java
+++ src/main/java/fr/openmc/core/features/trashbin/Trash.java
@@ -1,13 +1,15 @@
 package fr.openmc.core.features.trashbin;
 
-import fr.openmc.api.packetmenulib.menu.InventoryType;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public class Trash {
 
     private void trashBin(Player player) {
 
-        Inventory bin = Bukkit.createInventory(player, InventoryType.GENERIC_9X6,ChatColor.RED + "Poubelle");
+        Inventory bin = Bukkit.createInventory(player, 54, ChatColor.RED + "Poubelle");
 
         player.openInventory(bin);
     }
