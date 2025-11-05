e8797b463ab599ff9114096d3f725b4e94fde73a
Alexis LEBRUN <alexis@lebrun.ovh>
2025-11-05 21:42:24 +0100

    add required annotations

--- src/main/java/fr/openmc/core/features/trashbin/Trash.java
+++ src/main/java/fr/openmc/core/features/trashbin/Trash.java
@@ -4,10 +4,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.Description;
+import revxrsal.commands.bukkit.annotation.CommandPermission;
 
 public class Trash {
 
-    private void trashBin(Player player) {
+    @Command("trash")
+    @CommandPermission("omc.commands.trash")
+    @Description("Ouvre une poubelle pour jette les items")
+
+    private void trash(Player player) {
 
         Inventory bin = Bukkit.createInventory(player, 54, ChatColor.RED + "Poubelle");
 
