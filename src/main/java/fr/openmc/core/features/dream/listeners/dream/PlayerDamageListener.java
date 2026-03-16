ed5b84c2d856ca304a6d5182723d923b8cb6d170
iambibi_ <89582596+iambibi@users.noreply.github.com>
2026-03-16 18:55:46 +0100

    fix damage to other player

--- src/main/java/fr/openmc/core/features/dream/listeners/dream/PlayerDamageListener.java
+++ src/main/java/fr/openmc/core/features/dream/listeners/dream/PlayerDamageListener.java
@@ -1,9 +1,18 @@
 package fr.openmc.core.features.dream.listeners.dream;
 
+import fr.openmc.core.features.city.City;
+import fr.openmc.core.features.city.CityManager;
+import fr.openmc.core.features.city.ProtectionsManager;
+import fr.openmc.core.features.city.sub.mascots.utils.MascotUtils;
 import fr.openmc.core.features.dream.DreamUtils;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class PlayerDamageListener implements Listener {
@@ -14,6 +23,17 @@ public class PlayerDamageListener implements Listener {
         if (!(event.getEntity() instanceof Player player)) return;
 
         if (DreamUtils.isInDream(player)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        Entity victim = event.getEntity();
+        Entity damager = event.getDamager();
+
+        if (victim instanceof Player victimPlayer && DreamUtils.isInDreamWorld(victimPlayer)
+                && damager instanceof Player damagerPlayer && DreamUtils.isInDreamWorld(damagerPlayer)) {
             event.setCancelled(true);
         }
     }
