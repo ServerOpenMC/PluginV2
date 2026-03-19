4d63663028ca40e2883d59b2e160ce12dd0134de
iambibi_ <89582596+iambibi@users.noreply.github.com>
2026-03-19 21:07:46 +0100

    review

--- src/main/java/fr/openmc/core/features/dream/listeners/dream/PlayerDamageListener.java
+++ src/main/java/fr/openmc/core/features/dream/listeners/dream/PlayerDamageListener.java
@@ -1,16 +1,9 @@
 package fr.openmc.core.features.dream.listeners.dream;
 
-import fr.openmc.core.features.city.City;
-import fr.openmc.core.features.city.CityManager;
-import fr.openmc.core.features.city.ProtectionsManager;
-import fr.openmc.core.features.city.sub.mascots.utils.MascotUtils;
 import fr.openmc.core.features.dream.DreamUtils;
-import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -27,7 +20,7 @@ public class PlayerDamageListener implements Listener {
         }
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
+    @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
         Entity victim = event.getEntity();
         Entity damager = event.getDamager();
