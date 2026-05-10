e0d0756517b8b00a61e34930a298c79dce2356ec
iambibi_ <89582596+iambibi@users.noreply.github.com>
2026-05-10 11:10:50 +0200

    fix: CustomEnchantmentRegistry with args

--- src/main/java/fr/openmc/core/OMCRegistry.java
+++ src/main/java/fr/openmc/core/OMCRegistry.java
@@ -26,7 +26,7 @@ public final class OMCRegistry {
 
     public static void bootstrapAll(BootstrapContext context) {
         for (LifecycleRegistry r : OMCRegistry.ALL) {
-            if (isOverridden(r, "bootstrap")) {
+            if (isOverridden(r, "bootstrap", BootstrapContext.class)) {
                 r.bootstrap(context);
                 OMCLogger.successFormatted("Registre {} chargée pendant le bootstrap", r.getClass().getSimpleName());
             }
@@ -51,10 +51,10 @@ public final class OMCRegistry {
         }
     }
 
-    private static boolean isOverridden(LifecycleRegistry r, String methodName) {
+    private static boolean isOverridden(LifecycleRegistry r, String methodName, Class<?>... args) {
         try {
             return !r.getClass()
-                    .getMethod(methodName)
+                    .getMethod(methodName, args)
                     .getDeclaringClass()
                     .equals(LifecycleRegistry.class);
         } catch (NoSuchMethodException e) {
