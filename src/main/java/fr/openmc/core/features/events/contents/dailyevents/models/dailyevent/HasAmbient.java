eb1e5b3bc6f31cfc321b2a4e77bebb2973dabe1e
iambibi_ <89582596+iambibi@users.noreply.github.com>
2026-06-09 18:36:52 +0200

    build: fix

--- src/main/java/fr/openmc/core/features/events/contents/dailyevents/models/dailyevent/HasAmbient.java
+++ src/main/java/fr/openmc/core/features/events/contents/dailyevents/models/dailyevent/HasAmbient.java
@@ -3,6 +3,8 @@ package fr.openmc.core.features.events.contents.dailyevents.models.dailyevent;
 import fr.openmc.core.registry.ambient.CustomAmbient;
 import org.bukkit.entity.Player;
 
+import java.util.Collection;
+
 public interface HasAmbient {
     CustomAmbient getAmbient();
 
@@ -10,7 +12,15 @@ public interface HasAmbient {
         this.getAmbient().apply(player);
     }
 
+    default void apply(Collection<Player> receivers) {
+        this.getAmbient().apply(receivers);
+    }
+
     default void reset(Player player) {
         this.getAmbient().reset(player);
+    }
+
+    default void reset(Collection<Player> receivers) {
+        this.getAmbient().reset(receivers);
     }
 }
