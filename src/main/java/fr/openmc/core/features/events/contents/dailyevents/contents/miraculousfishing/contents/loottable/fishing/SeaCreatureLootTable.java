7b2ca630d8ba5b6a1a2c284ed26e6a398328d719
iambibi_ <89582596+iambibi@users.noreply.github.com>
2026-06-30 13:07:37 +0200

    change: add Kraken to SeaCreatureLootTable

--- src/main/java/fr/openmc/core/features/events/contents/dailyevents/contents/miraculousfishing/contents/loottable/fishing/SeaCreatureLootTable.java
+++ src/main/java/fr/openmc/core/features/events/contents/dailyevents/contents/miraculousfishing/contents/loottable/fishing/SeaCreatureLootTable.java
@@ -30,7 +30,8 @@ public class SeaCreatureLootTable extends CustomLootTable {
                 new SeaCreatureLoot(OMCRegistry.CUSTOM_MOBS.POISSON_STEVE, OMCRegistry.CUSTOM_ITEMS.POISSON_STEVE_HEAD, 0.3),
                 new SeaCreatureLoot(OMCRegistry.CUSTOM_MOBS.ANGRY_WITCH, 0.2),
                 new SeaCreatureLoot(OMCRegistry.CUSTOM_MOBS.ANCIENT_VILLAGER, 0.1),
-                new SeaCreatureLoot(OMCRegistry.CUSTOM_MOBS.LEVIATHAN, OMCRegistry.CUSTOM_ITEMS.LEVIATHAN_HEAD, 0.1)
+                new SeaCreatureLoot(OMCRegistry.CUSTOM_MOBS.LEVIATHAN, OMCRegistry.CUSTOM_ITEMS.LEVIATHAN_HEAD, 0.1),
+                new SeaCreatureLoot(OMCRegistry.CUSTOM_MOBS.KRAKEN, OMCRegistry.CUSTOM_ITEMS.KRAKEN_HEAD, 0.01)
         ));
     }
 }
