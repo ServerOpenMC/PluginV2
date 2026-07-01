ca9f8b8f0f521ae3f4d5f15154f6ab9368c55f37
Timeo Bavart <tbavart@gmail.com>
2026-07-01 02:36:37 +0200

    change: register feature has lambda

--- src/main/java/fr/openmc/core/OMCPlugin.java
+++ src/main/java/fr/openmc/core/OMCPlugin.java
@@ -93,7 +93,7 @@ public class OMCPlugin extends JavaPlugin {
             TransactionsManager::new,
             AnalyticsManager::new,
             FriendManager::new,
-            () -> new TabList(),
+            TabList::new,
             AdminShopManager::new,
             HelpConfigManager::new,
             BossbarManager::new,
