20684ace24de42acf3a4e9329e6b3545e7891ccc
Timeo Bavart <tbavart@gmail.com>
2026-07-12 12:09:04 +0200

    remove: open menu methods

--- src/main/java/fr/openmc/api/entity/player/OMCPlayer.java
+++ src/main/java/fr/openmc/api/entity/player/OMCPlayer.java
@@ -1,6 +1,5 @@
 package fr.openmc.api.entity.player;
 
-import fr.openmc.api.menulib.Menu;
 import fr.openmc.core.features.city.CityManager;
 import fr.openmc.core.features.economy.EconomyManager;
 import fr.openmc.core.utils.text.messages.MessageType;
@@ -12,11 +11,7 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 /**
  * Wrapper autour d'un {@link Player} pour les methodes propres a OpenMC
@@ -68,91 +63,6 @@ public class OMCPlayer implements Player {
         return player;
     }
 
-    public void open(@NotNull Menu menu) {
-        menu.open();
-    }
-
-    /**
-     * Construit puis ouvre un {@link Menu} pour ce joueur.
-     * Le joueur est automatiquement insere comme premier argument du constructeur :
-     * <pre>{@code
-     * player.open(CityChestMenu.class, city, page);
-     * // equivaut a : new CityChestMenu(player, city, page).open();
-     * }</pre>
-     *
-     * @param menuClass la classe du menu a ouvrir
-     * @param args      les arguments du constructeur, sans le joueur
-     * @return le menu ouvert
-     * @throws IllegalArgumentException si aucun constructeur public ne correspond aux arguments
-     */
-    public <T extends Menu> T open(@NotNull Class<T> menuClass, Object... args) {
-        for (Constructor<?> constructor : menuClass.getConstructors()) {
-            if (!matches(constructor, args)) {
-                continue;
-            }
-
-            Object[] fullArgs = new Object[args.length + 1];
-            fullArgs[0] = constructor.getParameterTypes()[0] == OMCPlayer.class ? this : player;
-            System.arraycopy(args, 0, fullArgs, 1, args.length);
-
-            try {
-                T menu = menuClass.cast(constructor.newInstance(fullArgs));
-                menu.open();
-                return menu;
-            } catch (InvocationTargetException e) {
-                throw new IllegalStateException("Erreur dans le constructeur de " + menuClass.getSimpleName(), e.getCause());
-            } catch (ReflectiveOperationException e) {
-                throw new IllegalStateException("Impossible d'instancier le menu " + menuClass.getSimpleName(), e);
-            }
-        }
-
-        String argTypes = Arrays.stream(args)
-                .map(arg -> arg == null ? "null" : arg.getClass().getSimpleName())
-                .collect(Collectors.joining(", "));
-        throw new IllegalArgumentException("Aucun constructeur public de " + menuClass.getSimpleName()
-                + " ne correspond a (Player" + (argTypes.isEmpty() ? "" : ", " + argTypes) + ")");
-    }
-
-    /**
-     * Verifie que le constructeur accepte un joueur en premier parametre,
-     * suivi des arguments donnes.
-     */
-    private static boolean matches(Constructor<?> constructor, Object[] args) {
-        Class<?>[] params = constructor.getParameterTypes();
-        if (params.length != args.length + 1) {
-            return false;
-        }
-        if (!params[0].isAssignableFrom(Player.class) && params[0] != OMCPlayer.class) {
-            return false;
-        }
-        for (int i = 0; i < args.length; i++) {
-            if (args[i] == null) {
-                if (params[i + 1].isPrimitive()) {
-                    return false;
-                }
-            } else if (!wrap(params[i + 1]).isInstance(args[i])) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Retourne la classe wrapper d'un type primitif (int -> Integer...),
-     * ou le type lui-meme s'il n'est pas primitif.
-     */
-    private static Class<?> wrap(Class<?> type) {
-        if (!type.isPrimitive()) return type;
-        if (type == int.class) return Integer.class;
-        if (type == long.class) return Long.class;
-        if (type == double.class) return Double.class;
-        if (type == float.class) return Float.class;
-        if (type == boolean.class) return Boolean.class;
-        if (type == short.class) return Short.class;
-        if (type == byte.class) return Byte.class;
-        return Character.class;
-    }
-
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof OMCPlayer omcPlayer) {
@@ -168,7 +78,10 @@ public class OMCPlayer implements Player {
 
     @Override
     public String toString() {
-        return "OMCPlayer{" + player.getName() + "}";
+        return "OMCPlayer{" +
+                "username=" + player.getDisplayName() +
+                ", uuid=" + player.getUniqueId() +
+                "}";
     }
 
     private final Message message = new Message();
