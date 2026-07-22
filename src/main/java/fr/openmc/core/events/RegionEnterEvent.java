03da8a6eca57fd43830bd8ea9f2604f73a89fbd7
iambibi_ <89582596+iambibi@users.noreply.github.com>
2026-07-22 16:34:22 +0200

    feat: add RegionEnterEvent and RegionLeaveEvent

---
+++ src/main/java/fr/openmc/core/events/RegionEnterEvent.java
@@ -0,0 +1,28 @@
+package fr.openmc.core.events;
+
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+public class RegionEnterEvent extends PlayerEvent {
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    private final ProtectedRegion region;
+    public RegionEnterEvent(ProtectedRegion region, Player player) {
+        super(player);
+        this.region = region;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
+}
---
+++ src/main/java/fr/openmc/core/events/RegionLeaveEvent.java
@@ -0,0 +1,28 @@
+package fr.openmc.core.events;
+
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+public class RegionLeaveEvent extends PlayerEvent {
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    private final ProtectedRegion region;
+    public RegionLeaveEvent(ProtectedRegion region, Player player) {
+        super(player);
+        this.region = region;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
+}
