36884e97fbce6e904c6b4b5347d89d298bc26b36
iambibi_ <89582596+iambibi@users.noreply.github.com>
2026-07-28 11:15:44 +0200

    fix: main hologram

--- src/main/java/fr/openmc/core/features/milestones/tutorial/TutorialHologram.java
+++ src/main/java/fr/openmc/core/features/milestones/tutorial/TutorialHologram.java
@@ -1,25 +1,18 @@
 package fr.openmc.core.features.milestones.tutorial;
 
 import fr.openmc.core.features.displays.holograms.Hologram;
-import fr.openmc.core.utils.text.fonts.CustomFonts;
 import fr.openmc.core.utils.text.messages.TranslationManager;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 
 public class TutorialHologram extends Hologram {
 
     public TutorialHologram() {
         super("tutorial");
 
-        String icon = CustomFonts.getBest("omc_icons:openmc", "");
         this.setLines(
-                TranslationManager.translation(
-                        "feature.milestones.tutorial.hologram.icon",
-                        Component.text(icon).color(NamedTextColor.WHITE)
-                ),
+                TranslationManager.translation("feature.milestones.tutorial.hologram.icon"),
                 TranslationManager.translation("feature.milestones.tutorial.hologram.welcome"),
                 TranslationManager.translation("feature.milestones.tutorial.hologram.based_on"),
-                Component.empty(),
+                TranslationManager.translation("empty"),
                 TranslationManager.translation("feature.milestones.tutorial.hologram.how_to"),
                 TranslationManager.translation("feature.milestones.tutorial.hologram.server"),
                 TranslationManager.translation("feature.milestones.tutorial.hologram.separator"),
