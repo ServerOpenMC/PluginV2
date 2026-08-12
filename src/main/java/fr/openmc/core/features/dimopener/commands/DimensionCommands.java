13ed21b5cac7eac4764d27765c822cdca126c68a
iambibi_ <89582596+iambibi@users.noreply.github.com>
2026-08-12 16:41:51 +0200

    Ajout d'un /dimension bypass (#1429)
    feat: add /dimension bypass

--- src/main/java/fr/openmc/core/features/dimopener/commands/DimensionCommands.java
+++ src/main/java/fr/openmc/core/features/dimopener/commands/DimensionCommands.java
@@ -1,9 +1,16 @@
 package fr.openmc.core.features.dimopener.commands;
 
+import fr.openmc.core.features.dimopener.DimensionOpenerManager;
 import fr.openmc.core.features.dimopener.menu.DimensionListMenu;
+import fr.openmc.core.utils.text.messages.MessageType;
+import fr.openmc.core.utils.text.messages.MessagesManager;
+import fr.openmc.core.utils.text.messages.Prefix;
+import fr.openmc.core.utils.text.messages.TranslationManager;
 import org.bukkit.entity.Player;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.CommandPlaceholder;
+import revxrsal.commands.annotation.Subcommand;
+import revxrsal.commands.bukkit.annotation.CommandPermission;
 
 @Command({"dimension", "dimensions", "dimopener"})
 public class DimensionCommands {
@@ -11,5 +18,18 @@ public class DimensionCommands {
     @CommandPlaceholder()
     public void openMenu(Player player) {
         new DimensionListMenu(player).open();
+    }
+
+    @Subcommand("bypass")
+    @CommandPermission("omc.admins.commands.dimopener.bypass")
+    public void bypass(Player player) {
+        if (DimensionOpenerManager.hasBypass(player)) {
+            DimensionOpenerManager.removeBypass(player);
+            MessagesManager.sendMessage(player, TranslationManager.translation("feature.dimopener.command.unbypass"), Prefix.STAFF, MessageType.SUCCESS, false);
+            return;
+        }
+
+        DimensionOpenerManager.addBypass(player);
+        MessagesManager.sendMessage(player, TranslationManager.translation("feature.dimopener.command.bypass"), Prefix.STAFF, MessageType.SUCCESS, false);
     }
 }
