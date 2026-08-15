45deafb97bbbf9949556c11a0d49b8a6af3f18d2
iambibi_ <89582596+iambibi@users.noreply.github.com>
2026-08-15 17:43:31 +0200

    fix: missing dimension register XXDDDDD

--- src/main/java/fr/openmc/core/registry/worldtemplates/WorldTemplateRegistry.java
+++ src/main/java/fr/openmc/core/registry/worldtemplates/WorldTemplateRegistry.java
@@ -1,7 +1,9 @@
 package fr.openmc.core.registry.worldtemplates;
 
 import fr.openmc.api.datapacks.OMCDatapack;
+import fr.openmc.api.datapacks.builders.dimensions.VoidDimensionBuilder;
 import fr.openmc.api.datapacks.injectors.BiomesInjector;
+import fr.openmc.api.datapacks.injectors.DimensionInjector;
 import fr.openmc.api.datapacks.injectors.DimensionTypesInjector;
 import fr.openmc.core.bootstrap.registries.KeyedRegistry;
 import fr.openmc.core.bootstrap.registries.Registry;
@@ -23,8 +25,16 @@ public class WorldTemplateRegistry extends Registry<String, WorldTemplate>
         for (WorldTemplate template : values()) {
             OMCDatapack worldTemplateDatapack = new OMCDatapack(template.getNamespace());
 
-            worldTemplateDatapack.addInjector(new DimensionTypesInjector(template.getNamespace(), template.getId(), template.dimensionType()));
-            worldTemplateDatapack.addInjector(new BiomesInjector(template.getNamespace(), template.getId(), template.biome()));
+            DimensionTypesInjector dimTypeInjector = new DimensionTypesInjector(template.getNamespace(), template.getId(), template.dimensionType());
+            worldTemplateDatapack.addInjector(dimTypeInjector);
+            BiomesInjector biomeInjector = new BiomesInjector(template.getNamespace(), template.getId(), template.biome());
+            worldTemplateDatapack.addInjector(biomeInjector);
+            worldTemplateDatapack.addInjector(new DimensionInjector(
+                    template.getNamespace(),
+                    template.getId(),
+                    new VoidDimensionBuilder()
+                            .biome(biomeInjector)
+                            .type(dimTypeInjector)));
 
             worldTemplateDatapack.buildBootstrap(context, true); // todo: remettre sur false qd fini de debug
         }
